e67d34cd83a3f630c4c9637f0d2c8366e1551feb
JBlixems <jfmalan123@gmail.com>
2026-05-17 21:02:28 +0200

    Big test

--- data_preparation/validate_simulate.py
+++ data_preparation/validate_simulate.py
@@ -122,7 +122,7 @@ def check_dataset(path, n_plot=4):
                           extent=[0, n_time, freq_min, freq_max],
                           vmin=vmin_c, vmax=vmax_c, cmap="plasma")
         rfi_rgba = np.zeros((*mask[i].T.shape, 4), dtype=np.float32)
-        rfi_rgba[mask[i].T > 0] = [0.0, 1.0, 0.2]
+        rfi_rgba[mask[i].T > 0] = [0.0, 1.0, 0.2, 1.0]  # bright green for masked pixels
         axes[i, 1].imshow(rfi_rgba, aspect="auto", origin="lower",
                           extent=[0, n_time, freq_min, freq_max])
         if i == 0:
