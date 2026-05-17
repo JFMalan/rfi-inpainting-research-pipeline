825976dc6dda89290aa4d3bbd9427a443b16edfe
JBlixems <jfmalan123@gmail.com>
2026-05-17 21:00:19 +0200

    Big test

--- data_preparation/validate_simulate.py
+++ data_preparation/validate_simulate.py
@@ -121,10 +121,10 @@ def check_dataset(path, n_plot=4):
         axes[i, 1].imshow(corrupted[i].T, aspect="auto", origin="lower",
                           extent=[0, n_time, freq_min, freq_max],
                           vmin=vmin_c, vmax=vmax_c, cmap="plasma")
-        rfi_overlay = np.where(mask[i].T > 0, 1.0, np.nan)
-        axes[i, 1].imshow(rfi_overlay, aspect="auto", origin="lower",
-                          extent=[0, n_time, freq_min, freq_max],
-                          vmin=0, vmax=1, cmap="cool", alpha=0.6)
+        rfi_rgba = np.zeros((*mask[i].T.shape, 4), dtype=np.float32)
+        rfi_rgba[mask[i].T > 0] = [0.0, 1.0, 0.2, 0.85]
+        axes[i, 1].imshow(rfi_rgba, aspect="auto", origin="lower",
+                          extent=[0, n_time, freq_min, freq_max])
         if i == 0:
             axes[i, 1].set_title("corrupted")
         axes[i, 2].imshow(mask[i].T, aspect="auto", origin="lower",
